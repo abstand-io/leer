ad0cae333155944a5c0b478cc8aa15090969ecc8
Sebastian Werler <seba.werler@gmail.com>
2020-04-23 22:08:50 +0200

    #2 minor adjustments to credit page

--- src/routes/credits/index.js
+++ src/routes/credits/index.js
@@ -16,29 +16,26 @@ export default class Home extends Component {
 			<div class={style.credits}>
 				<p class="lead">
 					<span class="title">Credits</span><br />
-					leer.im ist <a href="https://github.com/abstand-io/leer" target="_blank">Open Source Software</a> von abstand.io, 
-					gebaut von <a href="https://twitter.com/seveves" target="_blank">@seveves</a> und <a href="https://twitter.com/zebarto" target="_blank">@zebarto</a>. 
+					leer.im ist <a href="https://github.com/abstand-io/leer" target="_blank">Open Source Software</a>, gebaut 
+					von <a href="https://twitter.com/seveves" target="_blank">@seveves</a> und <a href="https://twitter.com/zebarto" target="_blank">@zebarto</a>. 
 					Die den angezeigten Ergebnissen zugrunde liegenden
 					Daten werden von Google bereitgestellt und
 					von uns abgerufen. Wir übernehmen keinerlei Haftung 
 					oder Gewährleistung für die Richtigkeit der Daten.
 				</p>
 				<p class="lead">
-					Ein Dank geht an <a href="https://maps.google.com" target="_blank">Google</a>, <a href="https://preactjs.com" target="_blank">preact</a>, 
-					<a href="https://undraw.co" target="_blank">undraw.co</a> und <a href="https://now.sh" target="_blank">now.sh</a>. 
+					Ein Dank geht an <a href="https://maps.google.com" target="_blank">Google</a>, <a href="https://preactjs.com" target="_blank">preact</a>, <a href="https://undraw.co" target="_blank"> undraw.co</a> und <a href="https://now.sh" target="_blank">now.sh</a>. 
 				</p>
 				<p class="lead">
 					Die Nutzung ist für Euch kostenfrei. Der Betrieb von
-					 leer kostet uns. Eine Spende zur
-					  Aufrechterhaltung des Dienstes erheitert die Betreiber ;)
+					 leer kostet uns. Eine Spende zur Aufrechterhaltung des Dienstes erheitert die Betreiber ;)
 				</p>
 
-				<img src="./assets/undraw_transfer_money_rywa.svg" style="width:50%;margin:auto;"/>
+				<img src="./assets/undraw_transfer_money_rywa.svg" style=""/>
 				
 				<div class={style.group}>
 					<button class="btn primary" onClick={this.openExternal}>Jetzt spenden</button>
 				</div>			
-				<p class={style.safe}>#staysafe</p>
 			</div>
 		);
 	}
